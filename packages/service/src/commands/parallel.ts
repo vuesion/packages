ff0b9878b967f4da243f7545fe1d4f66e6a1c920
Johannes Werner <johannes.werner86@gmail.com>
2019-02-23 15:46:43 +0100

    fix(service): handle error in parallel task

--- packages/service/src/commands/parallel.ts
+++ packages/service/src/commands/parallel.ts
@@ -1,5 +1,6 @@
 import { Command, ICommandHandler } from '../lib/command';
-import { runProcess } from '../utils/process';
+import { handleProcessError, runProcess } from '../utils/process';
+import { Spinner } from '../utils/ui';
 
 @Command({
   name: 'parallel [commands...]',
@@ -9,10 +10,37 @@ import { runProcess } from '../utils/process';
 export class Add implements ICommandHandler {
   public async run(args: string[], silent: boolean) {
     const promises = [];
+    const startTime: number = Date.now();
+    const spinner = new Spinner();
+    const max = args.length;
+    let done = 0;
+
+    const setSpinnerMessage = () => {
+      if (done === 3) {
+        spinner.message = `Finished running tasks in ${Date.now() - startTime}ms`;
+      } else {
+        spinner.message = `Running tasks ${done}/${max} ...`;
+      }
+    };
+
+    spinner.start();
+    setSpinnerMessage();
 
     args.forEach((command: string) => {
       const split = command.split(' ');
-      promises.push(runProcess(split.shift(), split));
+      promises.push(
+        runProcess(split.shift(), split, { silent: true }).then(() => {
+          done = done + 1;
+          setSpinnerMessage();
+        }),
+      );
     });
+
+    try {
+      await Promise.all(promises);
+      spinner.stop();
+    } catch (e) {
+      handleProcessError(e, spinner);
+    }
   }
 }
