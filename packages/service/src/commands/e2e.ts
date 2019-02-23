9dcb67e58843fb2881d94fad5c60ce8d3ce0a9a3
Johannes Werner <johannes.werner86@gmail.com>
2019-02-23 15:47:45 +0100

    refactor(service): remove defaults for test tasks

--- packages/service/src/commands/e2e.ts
+++ packages/service/src/commands/e2e.ts
@@ -3,11 +3,11 @@ import { handleProcessError, runProcess } from '../utils/process';
 
 @Command({
   name: 'e2e',
-  description: 'Run e2e tests with cypress.io.',
+  description: 'Run e2e tests with cypress.io. All cypress CLI options are supported.',
 })
 export class E2E implements ICommandHandler {
   public async run(args: string[], silent: boolean) {
-    args = args.concat(['run', '--browser', 'chrome']);
+    args = args.concat(['run']);
 
     try {
       await runProcess('cypress', args, { silent });
