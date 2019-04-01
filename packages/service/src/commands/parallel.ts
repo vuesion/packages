37e4f06d47bed143d8abc6c4281cf83532cac1a1
Johannes Werner <johannes.werner86@gmail.com>
2019-04-01 12:58:08 +0200

    feat(service): use concurrently for parallel command
    closes #9

--- packages/service/src/commands/parallel.ts
+++ packages/service/src/commands/parallel.ts
@@ -1,6 +1,7 @@
 import { Command, ICommandHandler, IRunOptions } from '../lib/command';
-import { handleProcessError, runProcess } from '../utils/process';
-import { Spinner } from '../utils/ui';
+import { handleProcessError } from '../utils/process';
+
+const concurrently = require('concurrently');
 
 @Command({
   name: 'parallel [commands...]',
@@ -8,42 +9,11 @@ import { Spinner } from '../utils/ui';
   description: 'Run commands in parallel.',
 })
 export class Add implements ICommandHandler {
-  private promises = [];
-  private startTime = Date.now();
-  private spinner = new Spinner();
-  private max = 0;
-  private done = 0;
-
-  private setSpinnerMessage() {
-    if (this.done === this.max) {
-      this.spinner.message = `Finished running tasks in ${Date.now() - this.startTime}ms`;
-    } else {
-      this.spinner.message = `Running tasks ${this.done}/${this.max} ...`;
-    }
-  }
-
   public async run(args: string[], options: IRunOptions) {
-    this.max = args.length;
-    this.done = 0;
-
-    this.spinner.start(options.debug);
-    this.setSpinnerMessage();
-
-    args.forEach((command: string) => {
-      const split = command.split(' ');
-      this.promises.push(
-        runProcess(split.shift(), split, { silent: true, ...options }).then(() => {
-          this.done = this.done + 1;
-          this.setSpinnerMessage();
-        }),
-      );
-    });
-
     try {
-      await Promise.all(this.promises);
-      this.spinner.stop();
+      await concurrently(args);
     } catch (e) {
-      handleProcessError(e, this.spinner);
+      handleProcessError(e);
     }
   }
 }
