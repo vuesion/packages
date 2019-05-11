7036db1d5f0893a75d64815e73dc4dfb7e8bad38
Johannes Werner <johannes.werner86@gmail.com>
2019-05-11 21:39:22 +0200

    feat: add addon-theme-switcher (#30)
    related to https://github.com/vuesion/vuesion/issues/391

---
+++ packages/addon-theme-switcher/src/ThemeSwitcher.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { SET_STORIES } from '@storybook/core-events';
+// @ts-ignore
+import { Icons, IconButton, WithTooltip, TooltipLinkList } from '@storybook/components';
+import { styled } from '@storybook/theming';
+
+const createItem = (id: string, title: string, change: any) => ({
+  id,
+  title,
+  onClick: () => {
+    change({ selected: { id, title }, expanded: false });
+    (document as any).querySelector('#storybook-preview-iframe').contentDocument.documentElement.className = id;
+  },
+});
+const IconButtonWithLabel: any = styled(IconButton)(() => ({
+  display: 'inline-flex',
+  alignItems: 'center',
+}));
+const IconButtonLabel: any = styled.div(({ theme }) => ({
+  fontSize: theme.typography.size.s2 - 1,
+  marginLeft: '10px',
+}));
+
+export class ThemeSwitcher extends React.Component<any, any> {
+  public listener: any;
+  public document: any = null;
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      themes: [],
+      selected: null,
+      expanded: false,
+    };
+
+    this.listener = () => {
+      const parameters = props.api.getCurrentStoryData().parameters;
+      let themes = (parameters.themeSwitcher && parameters.themeSwitcher.themes) || [];
+
+      themes = themes.map(({ label, value }: any) => createItem(value, label, this.change));
+
+      if (themes.length === 0) {
+        return;
+      }
+
+      this.setState({
+        themes,
+        selected: themes[0],
+      });
+
+      (document as any).querySelector('#storybook-preview-iframe').contentDocument.documentElement.className =
+        themes[0].id;
+    };
+  }
+
+  // @ts-ignore
+  public change = (...args: any) => this.setState(...args);
+
+  public componentDidMount() {
+    const { api } = this.props;
+    api.on(SET_STORIES, this.listener);
+  }
+
+  public render() {
+    const { selected, themes, expanded } = this.state;
+    return themes.length > 0 ? (
+      <WithTooltip
+        placement="top"
+        trigger="click"
+        tooltipShown={expanded}
+        onVisibilityChange={(s: any) => this.setState({ expanded: s })}
+        tooltip={<TooltipLinkList links={themes} />}
+        closeOnClick
+      >
+        <IconButtonWithLabel key="theme" title="Change the application theme">
+          <Icons icon="eye" />
+          <IconButtonLabel>{selected.title}</IconButtonLabel>
+        </IconButtonWithLabel>
+      </WithTooltip>
+    ) : null;
+  }
+}
