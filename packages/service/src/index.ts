a7ac02260ba5e10f29da8268febd702aea6293d0
Johannes Werner <johannes.werner86@gmail.com>
2019-07-21 21:56:58 +0200

    fix: npx usage

--- packages/service/src/index.ts
+++ packages/service/src/index.ts
@@ -2,21 +2,22 @@ import * as commander from 'commander';
 import { sync } from 'glob';
 import { packagesRoot } from '@vuesion/utils/dist/path';
 import { TSConfig } from '@vuesion/models';
-import { VuesionPackage } from '@vuesion/models';
-import { ServicePackage } from '@vuesion/models';
+import { VuesionPackage, ServicePackage } from '@vuesion/models';
 
 sync(`${packagesRoot('service')}/dist/commands/*.js`).forEach((file: string) => require(file));
 
-commander
-  .name('vuesion')
-  .version(ServicePackage.version, '-v, --version')
-  .option('-d, --debug', 'Show debugging output.', false)
-  .description(ServicePackage.description);
+commander.name('vuesion').option('-d, --debug', 'Show debugging output.', false);
 
 /**
- * update tsconfig with latest aliases
+ * not available during npx execution
  */
-TSConfig.updateCompilerOptionsPaths();
-VuesionPackage.updateModuleNameMapper();
+if (ServicePackage) {
+  commander.version(ServicePackage.version, '-v, --version').description(ServicePackage.description);
+  /**
+   * update tsconfig with latest aliases
+   */
+  TSConfig.updateCompilerOptionsPaths();
+  VuesionPackage.updateModuleNameMapper();
+}
 
 commander.parse(process.argv);
