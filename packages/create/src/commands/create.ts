08adfa6be35543427708651656ed2e119c05bb70
Johannes Werner <johannes.werner86@gmail.com>
2019-07-27 18:23:59 +0200

    feat(create): add post-install hook (#44)

--- packages/create/src/commands/create.ts
+++ packages/create/src/commands/create.ts
@@ -1,5 +1,5 @@
-import { Command, ICommandHandler, IRunOptions } from '../../../service/src/decorators/command';
 import chalk from 'chalk';
+import { Command, ICommandHandler, IRunOptions } from '../../../service/src/decorators/command';
 import { handleProcessError, runProcess, runtimeRoot, Spinner } from '../../../utils/src';
 
 const download = require('download-git-repo');
@@ -8,38 +8,65 @@ const download = require('download-git-repo');
   description: 'Create a new vuesion project.',
   arguments: [{ name: 'name', required: true }],
   options: [
-    { flags: '-n, --next', description: 'Download latest version.' },
+    { flags: '-n, --next', description: 'Download latest version.', defaultValue: false },
     { flags: '-d, --debug', description: 'Show debugging output.', defaultValue: false },
   ],
 })
 export class Create implements ICommandHandler {
+  private spinner = new Spinner();
+
   public name: string;
   public next: boolean;
   public debug: boolean;
 
+  private download(branch, destination) {
+    return new Promise((resolve, reject) => {
+      download(branch, destination, async (e: any) => {
+        if (e) {
+          reject({ code: 1, trace: e.toString() });
+        }
+        resolve();
+      });
+    });
+  }
+
+  private async install(options) {
+    this.spinner.message = 'Installing dependencies...';
+
+    await runProcess('npm', ['install'], { silent: true, ...options });
+  }
+
+  private async postInstall(destination, options) {
+    this.spinner.message = 'Running post-install...';
+
+    await runProcess(
+      destination + '/node_modules/.bin/vuesion',
+      ['post-install', JSON.stringify({ name: this.name, branch: this.next ? 'next' : 'master' })],
+      {
+        silent: true,
+        ...options,
+      },
+    );
+  }
+
   public async run(args: string[], options: IRunOptions) {
     const destination = runtimeRoot(this.name);
     const branch = this.next ? 'github:vuesion/vuesion#next' : 'github:vuesion/vuesion';
-    const spinner = new Spinner();
 
-    spinner.message = 'Downloading project...';
-    spinner.start(options.debug);
+    this.spinner.message = 'Downloading project...';
+    this.spinner.start(options.debug);
+    try {
+      await this.download(branch, destination);
 
-    download(branch, destination, async (e: any) => {
-      if (e) {
-        handleProcessError({ code: 1, trace: e.toString() }, spinner);
-      }
+      process.chdir(destination);
 
-      spinner.message = 'Installing dependencies...';
+      await this.install(options);
+      await this.postInstall(destination, options);
 
-      try {
-        await runProcess('npm', ['install'], { cwd: destination, silent: true, ...options });
-
-        spinner.message = `Project ${chalk.bold(this.name)} successfully created`;
-        spinner.stop();
-      } catch (err) {
-        handleProcessError(e, spinner);
-      }
-    });
+      this.spinner.message = `Project ${chalk.bold(this.name)} successfully created`;
+      this.spinner.stop();
+    } catch (e) {
+      handleProcessError(e, this.spinner);
+    }
   }
 }
---
+++ packages/service/src/commands/post-install.ts
@@ -0,0 +1,44 @@
+import { sync } from 'rimraf';
+import { Command, ICommandHandler, IRunOptions } from '../decorators/command';
+import { VuesionConfig, VuesionPackage } from '@vuesion/models/dist';
+
+@Command({
+  name: 'post-install',
+  description: 'Separate steps that run after creating a new vuesion app.',
+  arguments: [{ name: 'options', required: true }],
+})
+export class PostInstall implements ICommandHandler {
+  private optionsObject: { name: string; branch: string };
+  public options: string;
+
+  private deleteDirectories() {
+    ['./.git', './.circleci', './.all-contributorsrc', './CHANGELOG.md', './CODE_OF_CONDUCT.md', './LICENSE'].forEach(
+      (glob) => sync(glob),
+    );
+  }
+
+  private updateVuesionConfig() {
+    VuesionConfig.updateCurrentVersion(`v${VuesionPackage.version}`);
+  }
+
+  private updateVuesionPackage() {
+    VuesionPackage.name = this.optionsObject.name;
+    VuesionPackage.version = '0.0.0';
+    VuesionPackage.description = 'Created with vuesion.';
+    VuesionPackage.repository = { type: '', url: '' };
+    VuesionPackage.keywords = [];
+    VuesionPackage.author = '';
+    VuesionPackage.homepage = '';
+    VuesionPackage.bugs = { url: '' };
+
+    VuesionPackage.save(true);
+  }
+
+  public async run(args: string[], options: IRunOptions) {
+    this.optionsObject = JSON.parse(this.options);
+
+    this.deleteDirectories();
+    this.updateVuesionConfig();
+    this.updateVuesionPackage();
+  }
+}
