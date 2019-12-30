b968f647e79ec4faf5fcd8583a4d5859030dda1a
Johannes Werner <johannes.werner86@gmail.com>
2019-12-30 03:49:01 +0100

    feat: add addon-vuetify (#56)

---
+++ packages/addon-vuetify/src/App.ts
@@ -0,0 +1,39 @@
+import { ObjectLiteralExpression, SyntaxKind } from 'ts-morph';
+import { runtimeRoot } from '@vuesion/utils';
+import { ASTModel } from '@vuesion/models';
+
+export class App extends ASTModel {
+  constructor() {
+    super(runtimeRoot('src/app/app.ts'));
+  }
+
+  public transform() {
+    this.load();
+
+    const importCount = this.sourceFile.getChildrenOfKind(SyntaxKind.ImportDeclaration).length;
+    this.sourceFile.insertStatements(importCount, `import Vuetify from 'vuetify';`);
+    this.sourceFile.insertStatements(importCount + 1, `\nVue.use(Vuetify);`);
+
+    const options: ObjectLiteralExpression = this.sourceFile
+      .getFirstChildByKind(SyntaxKind.VariableStatement)
+      .getFirstChildByKind(SyntaxKind.VariableDeclarationList)
+      .getFirstChildByKind(SyntaxKind.VariableDeclaration)
+      .getFirstChildByKind(SyntaxKind.ArrowFunction)
+      .getFirstChildByKind(SyntaxKind.Block)
+      .getFirstChildByKind(SyntaxKind.VariableStatement)
+      .getFirstChildByKind(SyntaxKind.VariableDeclarationList)
+      .getFirstChildByKind(SyntaxKind.VariableDeclaration)
+      .getFirstChildByKind(SyntaxKind.NewExpression)
+      .getFirstChildByKind(SyntaxKind.ObjectLiteralExpression);
+    const pluginIndex = options.getChildrenOfKind(SyntaxKind.ShorthandPropertyAssignment).length;
+    const vuetify = `vuetify: new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+}),`;
+
+    options.insertProperty(pluginIndex, vuetify);
+
+    this.save();
+  }
+}
