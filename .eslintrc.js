e4c926ae19277d08c03c7d0d1823159e37d20829
Johannes Werner <johannes.werner@hey.com>
2022-05-26 21:09:24 +0200

    chore: update main repo deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,9 +18,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
     'plugin:vue/recommended',
+    'prettier',
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
